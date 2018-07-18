91960be805a758f2ef351ced07d00c9638cf27f4
neelamari <neelamari@icloud.com>
2018-07-18 16:49:45 -0600

    movie fun lab

--- src/main/java/org/superbiz/moviefun/AlbumsDatabaseConfig.java
+++ src/main/java/org/superbiz/moviefun/AlbumsDatabaseConfig.java
@@ -1,4 +1,48 @@
 package org.superbiz.moviefun;
 
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+@Configuration
 public class AlbumsDatabaseConfig {
+
+    @Bean
+    public DataSource albumsDataSource(DatabaseServiceCredentials serviceCredentials) {
+
+      //  MysqlDataSource dataSource = new MysqlDataSource();
+
+        HikariDataSource dataSource = new HikariDataSource();
+     //   dataSource.setJdbcUrl(serviceCredentials.)
+//d
+        dataSource.setJdbcUrl(serviceCredentials.jdbcUrl("albums-mysql", "p-mysql"));
+
+
+      //  dataSource.setURL(serviceCredentials.jdbcUrl("albums-mysql", "p-mysql"));
+        return dataSource;
+    }
+
+    @Bean
+    LocalContainerEntityManagerFactoryBean albumsEntityManagerFactory(DataSource albumsDataSource, HibernateJpaVendorAdapter jpaVendorAdapter) {
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setDataSource(albumsDataSource);
+        factoryBean.setJpaVendorAdapter(jpaVendorAdapter);
+        factoryBean.setPackagesToScan(DatabaseConfig.class.getPackage().getName());
+        factoryBean.setPersistenceUnitName("albums");
+        return factoryBean;
+    }
+
+    @Bean
+    PlatformTransactionManager albumsTransactionManager(EntityManagerFactory albumsEntityManagerFactory) {
+        return new JpaTransactionManager(albumsEntityManagerFactory);
+    }
+
 }
